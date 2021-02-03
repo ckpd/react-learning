39a6cc3b80e6052fa7461909f22eec2974202dcf
Charlie DeRiggs <charlie.deriggs@gmail.com>
2021-02-02 21:36:29 -0400

    choose a winner

--- src/Board.js
+++ src/Board.js
@@ -44,45 +44,61 @@ class Board extends Component {
  
   }
 
+  renderSquare(i) {
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
+  }
+
 
    render(){
-    const status = `Next Player - ${this.state.isNextClickX   ? 'X': 'O'}`; //string  interpolation
+    console.log(this.state.squares);
 
+    let status;
+
+    const winner = calculateWinner(this.state.squares);
+    
+  
+    if (winner) {
+      status = 'Winner: ' + winner;    
+    } else {      
+      status = 'Next player: ' + 
+      (this.state.isNextClickX ? 'X' : 'O');   
+    }
+    
     return (
         <div>
           <h1>{status} </h1>
           <div className="board-row">
-              <Square value={this.state.squares[1]}
-                onClick = {()=>this.handleClick(1)} />
-
-              <Square value={this.state.squares[2]}
-                onClick = {()=>this.handleClick(2)} />
-
-              <Square value={this.state.squares[3]}
-                onClick = {()=>this.handleClick(3)} />
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
 
               <div className="board-row">
               </div>
+              <Square value={this.state.squares[3]} 
+               onClick = {()=>this.handleClick(3)} />
+
               <Square value={this.state.squares[4]} 
                onClick = {()=>this.handleClick(4)} />
 
               <Square value={this.state.squares[5]} 
-               onClick = {()=>this.handleClick(5)} />
-
-              <Square value={this.state.squares[6]} 
-                onClick = {()=>this.handleClick(6)} />
+                onClick = {()=>this.handleClick(5)} />
 
 
               <div className="board-row">
               </div>
+              <Square value={this.state.squares[6]} 
+               onClick = {()=>this.handleClick(6)} />
+
               <Square value={this.state.squares[7]} 
                onClick = {()=>this.handleClick(7)} />
 
               <Square value={this.state.squares[8]} 
-               onClick = {()=>this.handleClick(8)} />
-
-              <Square value={this.state.squares[9]} 
-                onClick = {()=>this.handleClick(9)} />
+                onClick = {()=>this.handleClick(8)} />
               </div>
 
         </div>
@@ -90,5 +106,35 @@ class Board extends Component {
   }
  
 }
+
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+     
+    const [a, b, c] = lines[i]; 
+
+    console.log(` squares[a]  ${squares[a]}  \t  squares[b] ${squares[b]}  \t  squares[c] ${squares[c]} `);
+
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) { 
+      console.log(`what the fuck ${squares[a]}`);
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+
 
 export default Board;
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import Board from './Board.js'
  
 ReactDOM.render(
-    <Board/>
-   , document.getElementById('root')
+    <Board/>,
+    document.getElementById('root')
 );
 
